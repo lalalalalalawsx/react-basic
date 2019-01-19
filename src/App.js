f60b3923b24c31b5293282efe9c152692b8a0744
wsx <2845336692@qq.com>
2019-01-19 09:07:38 +0800

    最终版完成

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-
+import 'common/style/index.scss';
+import 'common/style/common.scss';
+import 'common/fonts/iconfont.css';
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        HELLO
       </div>
     );
   }
